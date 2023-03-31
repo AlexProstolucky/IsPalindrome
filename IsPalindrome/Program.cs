e42cabc4b7a17e8ade465f1bb2699234db7afb98
AlexProstolucky <alexprostolucky@gmail.com>
2023-03-31 16:55:25 +0300

    Добавьте файлы проекта.

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿
+internal class Program
+{
+    public static bool IsPalindrome(string str)
+    {
+        int i = 0;
+        int j = str.Length - 1;
+
+        while (i < j)
+        {
+            if (str[i] != str[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Write text to check it:");
+        string text; 
+        text = Console.ReadLine();
+        bool Palindrom = IsPalindrome(text);
+        Console.WriteLine(Palindrom);
+        Console.ReadKey();
+    }
+}
